654a1100a9f30d7a265eea7700380032d7721ace
Craig bakke <casbakke@gmail.com>
2023-03-01 15:30:30 -0500

    added comments

--- Clicker Game.py
+++ Clicker Game.py
@@ -2,10 +2,12 @@ import tkinter as tk
 import threading, os, json
 from time import sleep
 
+# constants
 LIGHT_GRAY = "#e0e0e0"
 GRAY = "#c7c7c7"
 DARK_GRAY = "#969696"
 
+# load config file
 with open("config.json", "r") as f:
     config = json.load(f)
 
@@ -16,14 +18,19 @@ click_counter = [0, 0, 0, 0, 0]
 clicks_per_second = 0.0
 running = True
 
+# define & configure Tk object
 window = tk.Tk()
 window.title("Clicker Game")
 window.geometry("480x720+50+50")
 window.maxsize(480, 720)
 window.minsize(480, 720)
 
+# home screen
 def homescreen():
+
     global balance, cash_per_second, clicks_per_second
+
+    # updates all of the labels on screen
     def update_labels():
         global balance
 
@@ -33,6 +40,7 @@ def homescreen():
         lbl_cash_per_sec["text"] = f"${mps} per second"
         lbl_money["text"] = f"${balance}"
 
+    # swaps the screen to show a different interface
     def shop_clicked():
         def back_clicked():
             tk.Button(
@@ -42,18 +50,19 @@ def homescreen():
                 text = "Click Me",
                 font = ("Arial", 42),
                 command = clicker_clicked,
-            ).grid(row = 0, column = 0, ipadx = 50, ipady = 60)
+            ).grid(row = 0, column = 0, ipadx = 50, ipady = 60) # create a new clicker button
             btn_shop["text"] = "Shop"
             btn_shop["command"] = shop_clicked
 
-        frm_body.winfo_children()[0].destroy()
+        frm_body.winfo_children()[0].destroy() # hide the clicker button
         btn_shop["text"] = "Back"
         btn_shop["command"] = back_clicked
 
+    # save the user's data and exit the program
     def quit(bal, cpc, cps):
         global running, t1
-        running = False
-        sleep(1.1)
+        running = False # stop the other thread
+        sleep(1) # delay to ensure the other thread is not mid-iteration
         window.destroy()
 
         outfile = {
@@ -62,9 +71,11 @@ def homescreen():
             "cpc": cpc
         }
 
+        # store the current state of the important variables in a config file
         with open("config.json", "w") as f:
             json.dump(outfile, f)
 
+    # runs when the clicker button is pressed by the user
     def clicker_clicked():
         global balance, click_counter
         balance += cash_per_click
@@ -73,26 +84,31 @@ def homescreen():
         update_labels()
 
 
-    frm_head = tk.Frame(bg = GRAY)
-    frm_body = tk.Frame(bg = LIGHT_GRAY)
-    frm_foot = tk.Frame(bg = GRAY)
-    
+    # configure window
     window.columnconfigure(0, minsize = 480)
     window.rowconfigure(0, minsize = 140)
     window.rowconfigure(1, minsize = 480)
     window.rowconfigure(2, minsize = 100)
-    
-    frm_head.grid(row = 0, column = 0, sticky = "nesw")
-    frm_body.grid(row = 1, column = 0, sticky = "nesw")
-    frm_foot.grid(row = 2, column = 0, sticky = "nesw")
 
+    # define frames
+    frm_head = tk.Frame(bg = GRAY)
+    frm_body = tk.Frame(bg = LIGHT_GRAY)
+    frm_foot = tk.Frame(bg = GRAY)
+
+    # configure frames
     frm_head.columnconfigure(0, minsize = 480)
     frm_body.rowconfigure(0, minsize = 140)
     frm_body.columnconfigure(0, minsize = 480)
     frm_body.rowconfigure(0, minsize = 480)
     frm_foot.columnconfigure(0, minsize = 480)
     frm_foot.rowconfigure(0, minsize = 100)
+    
+    # place frames
+    frm_head.grid(row = 0, column = 0, sticky = "nesw")
+    frm_body.grid(row = 1, column = 0, sticky = "nesw")
+    frm_foot.grid(row = 2, column = 0, sticky = "nesw")
 
+    # define widgets
     lbl_money = tk.Label(
         frm_head,
         text = f"${balance}",
@@ -143,6 +159,7 @@ def homescreen():
         width = 5,
     )
 
+    # place widgets
     lbl_money.grid(row = 0, column = 0)
     lbl_cash_per_sec.grid(row = 1, column = 0)
     lbl_cash_per_click.grid(row = 2, column = 0)
@@ -151,17 +168,20 @@ def homescreen():
 
     btn_quit.place(x = 375, y = 25, height = 50)
 
+    # while loop that runs in the background on another thread
     def clock():
         global running
         while running:
             global balance, cash_per_second, click_counter, clicks_per_second
             sleep(1)
 
-            balance += cash_per_second
+            balance += cash_per_second # add {cash_per_second} to the user's balance once every second, this is the passive income system
 
+            # average out the clicks per second from the last 5 seconds
             sum = click_counter[0] + click_counter[1] + click_counter[2] + click_counter[3] + click_counter[4]
             clicks_per_second = round((float(sum) / 5.0), 1)
 
+            # move each value through a rotating list
             click_counter[4] = click_counter[3]
             click_counter[3] = click_counter[2]
             click_counter[2] = click_counter[1]
@@ -170,11 +190,14 @@ def homescreen():
 
             update_labels()
 
+    # use multithreading so that this can run in the background without interfering with mainloop
     global t1
     t1 = threading.Thread(target = clock)
 
-homescreen()
+### start of runtime ###
+
+homescreen() # show the homescreen
 global t1
-t1.start()
-window.mainloop()
+t1.start() # start the clock thread
+window.mainloop() # show the GUI window
 SystemExit()
